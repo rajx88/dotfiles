d417c73dd983e9e9136e26017f473b76af309fa6
rajx88 <44810778+rajx88@users.noreply.github.com>
2023-07-31 14:17:39 +0200

    adjusted text color to be visable

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require "wezterm"
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
+  local window = mux.spawn_window(cmd or {})
   window:gui_window():maximize()
 end)
 
@@ -24,6 +24,11 @@ end
 -- For example, changing the color scheme:
 -- config.color_scheme = "AdventureTime"
 
+-- Setting text color
+config.colors = {
+  foreground = "#FFFFFF"
+}
+-- set background opacity
 config.window_background_opacity = 0.8
 config.font = wezterm.font_with_fallback {
   "CaskaydiaCove Nerd Font",
