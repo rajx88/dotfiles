e27fba5c4cd3858472714cf885f6fd82a807ee53
rajx88 <44810778+rajx88@users.noreply.github.com>
2023-09-19 17:19:18 +0200

    changed nvim config

--- dot_config/nvim/lua/rajkohlen/core/settings.lua
+++ dot_config/nvim/lua/rajkohlen/core/settings.lua
@@ -1,3 +1,9 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
 -- settings colorscheme
 vim.cmd.colorscheme("rose-pine")
 -- disable background
